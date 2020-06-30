caae1511aad39694143009cdc543dd83878d5408
DzmitryZaitsau <dimlepus@gmail.com>
2020-06-30 17:20:53 +0300

    Init

---
+++ src/main/java/com/my/countryroom/CountryroomApplication.java
@@ -0,0 +1,15 @@
+package com.my.countryroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryroomApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CountryroomApplication.class, args);
+    }
+
+}
